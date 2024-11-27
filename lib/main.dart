cecbe1d3b12a711fdd3e6409336e95fe34ba76f3
notsukeisuke <notsukeisuke@gmail.com>
2024-11-27 23:55:39 +0000

    市区町村一覧のwidgetを切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Resus Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('市区町村一覧')),
-        body: const Center(
-          child: Text('市区町村の一覧画面です')),
-      )
-    );
+    return const MaterialApp(
+      title: 'Flutter Resus Demo', 
+      home: CityListPage());
   }
 }
 
+class CityListPage extends StatelessWidget {
+  const CityListPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('市区町村一覧')),
+      body: const Center(child: Text('市区町村の一覧画面です')),
+    );
+  }
+}
