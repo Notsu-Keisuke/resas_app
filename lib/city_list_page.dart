fe3df622a78df7ebc954064d145306aefc407429
notsukeisuke <notsukeisuke@gmail.com>
2024-11-28 04:42:20 +0000

    ナビゲーターを使って画面遷移を作成

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/city_detail_page.dart';
 
 class CityListPage extends StatelessWidget {
   const CityListPage({
@@ -24,13 +25,16 @@ class CityListPage extends StatelessWidget {
       appBar: AppBar(title: const Text('市区町村一覧')),
       body: ListView(
         children: [
-          for(final city in cities)
+          for (final city in cities)
             ListTile(
               title: Text(city),
               subtitle: const Text('政令指定都市'),
               trailing: const Icon(Icons.navigate_next),
               onTap: () {
                 //詳細画面に遷移する
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const CityDetailPage()),
+                );
               },
             ),
         ],
@@ -38,3 +42,4 @@ class CityListPage extends StatelessWidget {
     );
   }
 }
+
