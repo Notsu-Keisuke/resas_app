4001a46106b16d3587d8ec067a3d423b96afe0e4
notsukeisuke <notsukeisuke@gmail.com>
2024-11-29 00:02:06 +0000

    変更をほぞん

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/city_detail_page.dart';
+import 'package:http/http.dart' as http;
+import 'env.dart';
 
-class CityListPage extends StatelessWidget {
+class CityListPage extends StatefulWidget {
   const CityListPage({
     super.key,
   });
+
+  @override
+  State<CityListPage> createState() => _CityListPageState();
+}
+
+class _CityListPageState extends State<CityListPage> {
+  late Future<String> _citiesFuture;
+  @override
+  void initState() {
+    super.initState();
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = '/api/v1/cities';
+    final headers = {
+      'X-API-KEY': Env.resasApiKey,
+    };
+    _citiesFuture = http.get(
+      Uri.https(host, endpoint),
+      headers: headers,
+    ) as Future<String>;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,26 +44,43 @@ class CityListPage extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(title: const Text('市区町村一覧')),
-      body: ListView(
-        children: [
-          for (final city in cities)
-            ListTile(
-              title: Text(city),
-              subtitle: const Text('政令指定都市'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {
-                //詳細画面に遷移する
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => CityDetailPage(
-                            city: city,
-                          )),
-                );
-              },
-            ),
-        ],
+      appBar: AppBar(
+        title: const Text('市区町村一覧'),
       ),
-      );
+      body: FutureBuilder<String>(
+        future: _citiesFuture,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            // 非同期処理が完了（3秒後）したこと示す状態です。
+            case ConnectionState.done:
+              // 元々のListViewを移動させただけです
+              return ListView(
+                children: [
+                  for (final city in cities)
+                    ListTile(
+                      title: Text(city),
+                      subtitle: const Text('政令指定都市'),
+                      trailing: const Icon(Icons.navigate_next),
+                      onTap: () {
+                        Navigator.of(context).push<void>(
+                          MaterialPageRoute(
+                            builder: (context) => CityDetailPage(city: city),
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              );
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+          }
+          // 非同期処理が完了するまではインジケータを表示します。
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
